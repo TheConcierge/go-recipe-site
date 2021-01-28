1e3151a2bde43e121dbe7d3f8017076a83f1d714
Ryan Grant <rgrant9@vt.edu>
2021-01-27 20:45:06 -0800

    updates

--- core/service.go
+++ core/service.go
@@ -6,4 +6,6 @@ import "bytes"
 type RecipeService interface {
 	SingleRecipePage(id string) (*bytes.Buffer, error)
 	IndexPage() (*bytes.Buffer, error)
+    SearchRecipes(name string) (*bytes.Buffer, error)
+    Inject() (*bytes.Buffer, error)
 }
